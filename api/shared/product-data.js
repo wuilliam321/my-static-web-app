273bee88a7c47cb615df6bed3c0d7bf54b370bd5
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2022-03-08 16:43:55 -0300

    azure functions

---
+++ api/shared/product-data.js
@@ -0,0 +1,10 @@
+module.exports.getProducts = function() {
+    return [
+        {
+          id: 12,
+          name: 'Strawberries',
+          description: '16oz package of fresh organic strawberries',
+          quantity: '1',
+        },
+    ];
+}
